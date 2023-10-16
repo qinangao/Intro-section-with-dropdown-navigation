41f5c6468593662f8a353b41713303cdb8036726
Qinan Gao <qinanagao@gmail.com>
2023-10-16 22:43:44 +0100

    add dropdown function

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "250px";
+  document.getElementById("mySidepanel").style.width = "220px";
 }
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
+}
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+for (var i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
 }
